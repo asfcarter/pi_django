643b3a74c6660b5899df72101b8b1e8a31322ffe
asfcarter <asfcarter@yahoo.co.uk>
2015-09-16 02:32:54 +0100

    Link in the old pages

--- src/pi_django/views.py
+++ src/pi_django/views.py
@@ -1,4 +1,46 @@
 from django.views import generic
 
-class Index2Page(generic.TemplateView):
+class AccenturePage(generic.TemplateView):
+    template_name = "Accenture.html"
+
+class ANPRPage(generic.TemplateView):
+    template_name = "ANPR.html"
+
+class apachePage(generic.TemplateView):
+    template_name = "apache.html"
+
+class Bands_I_likePage(generic.TemplateView):
+    template_name = "Bands_I_like.html"
+
+#class C-optimizationPage(generic.TemplateView):
+#    template_name = "C-optimization.html"
+
+class cricketPage(generic.TemplateView):
+    template_name = "cricket.html"
+
+class cvPage(generic.TemplateView):
+    template_name = "cv.html"
+
+class djangoPage(generic.TemplateView):
+    template_name = "django.html"
+
+class duPage(generic.TemplateView):
+    template_name = "du.html"
+
+class Energy_bus_exercisesPage(generic.TemplateView):
+    template_name = "Energy_bus_exercises.html"
+
+class Energy_busPage(generic.TemplateView):
+    template_name = "Energy_bus.html"
+
+class gitPage(generic.TemplateView):
+    template_name = "git.html"
+
+class homePage(generic.TemplateView):
+    template_name = "home.html"
+
+class hostsPage(generic.TemplateView):
+    template_name = "hosts.html"
+
+class IndexPage(generic.TemplateView):
     template_name = "index.html"
