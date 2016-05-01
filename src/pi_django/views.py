e2526debb462ac393357ce48e9207b6fc2df483d
asfcarter <asfcarter@yahoo.co.uk>
2016-05-01 23:06:03 +0100

    Update with May objectives

--- src/pi_django/views.py
+++ src/pi_django/views.py
@@ -30,6 +30,9 @@ class duPage(generic.TemplateView):
 class Energy_bus_exercisesPage(generic.TemplateView):
     template_name = "html/Energy_bus_exercises.html"
 
+class Energy_bus_exercisesMay2016Page(generic.TemplateView):
+    template_name = "html/Energy_bus_exercises_May_2016.html"
+
 class Energy_bus_exercisesApr2016Page(generic.TemplateView):
     template_name = "html/Energy_bus_exercises_Apr_2016.html"
 
