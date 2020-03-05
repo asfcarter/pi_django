b2b0805105f31ed4beb913e6e2c2888be153a930
asfcarter <asfcarter@yahoo.co.uk>
2020-03-05 20:51:20 +0000

    Add March 2020

--- src/pi_django/views.py
+++ src/pi_django/views.py
@@ -30,6 +30,9 @@ class duPage(generic.TemplateView):
 class Energy_bus_exercisesPage(generic.TemplateView):
     template_name = "html/Energy_bus_exercises.html"
 
+class Energy_bus_exercisesMarch2020Page(generic.TemplateView):
+    template_name = "html/Energy_bus_exercises_March_2020.html"
+
 class Energy_bus_exercisesJan2019Page(generic.TemplateView):
     template_name = "html/Energy_bus_exercises_Jan_2019.html"
 
